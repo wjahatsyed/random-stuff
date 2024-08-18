11e38dfbf741467d06aa9640be14a47c0f3c9520
Wajahat Adil <wajahatsyed1257@gmail.com>
2024-08-18 12:53:15 +0500

    Anagram function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    private static String sortCharacter(String input) {
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public List<String> getAnagram(String[] array1, String[] array2) {
+        HashSet<String> sortedWordsInArrays2 = new HashSet<>();
+        for (String word : array2) {
+            sortedWordsInArrays2.add(sortCharacter(word));
+        }
+        HashSet<String> anagramsMatched = new HashSet<>();
+        ArrayList<String> result = new ArrayList<>();
+        for (String word : array1) {
+            if (sortedWordsInArrays2.contains(sortCharacter(word))) {
+                if (!anagramsMatched.contains(word)) {
+                    result.add(word);
+                    anagramsMatched.add(word);
+                }
+            }
+        }
+        return result;
     }
 }
